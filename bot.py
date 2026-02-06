552aa0947a850be3e265a30f4fa50a997779566e
andreyminyukevich-create <andreyminyukevich@gmail.com>
2026-02-06 16:17:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -442,7 +442,10 @@ async def finalize_kp(callback: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     photos = data.get("photos", [])
     car_data = data.get("car_data", {})
-    
+        user_name = (callback.from_user.first_name or "").strip()
+    if not user_name:
+        user_name = (callback.from_user.username or "Менеджер").strip()
+    car_data["user_name"] = user_name
     if len(photos) < 3:
         await callback.answer("⚠️ Нужно минимум 3 фото!", show_alert=True)
         return
